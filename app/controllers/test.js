d6f4f3337c1dffaf89f5f6624f6124ceb07c6186
liu_sf <1350985242@qq.com>
2019-01-21 10:34:44 +0800

    .

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -1,21 +1,41 @@
 var fs = require('fs')
 var path = require('path')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-var p1 = new Promise(function (resolve,reject) {
-    fs.readFile('a.txt','utf-8',function (err,data) {
-        if (err){
-            reject(err)
-        } else {
-            resolve(data)
-        }
-    })
-})
-
-p1.then(data => {
-    console.log(data)
-}).catch(err =>{
-    console.log("文件读取失败")
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbase = db.db("runoob");
+    var myobj = { name: "菜鸟教程", url: "www.runoob" };
+    //创建了site集合，也就是site表
+    // dbase.createCollection('site', function (err, res) {
+    //     if (err) throw err;
+    //     console.log("创建集合!");
+    //     db.close();
+    // });
+    dbase.collection("site").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("文档插入成功");
+        db.close();
+    });
 });
+
+
+// var p1 = new Promise(function (resolve,reject) {
+//     fs.readFile('a.txt','utf-8',function (err,data) {
+//         if (err){
+//             reject(err)
+//         } else {
+//             resolve(data)
+//         }
+//     })
+// })
+//
+// p1.then(data => {
+//     console.log(data)
+// }).catch(err =>{
+//     console.log("文件读取失败")
+// });
 
 // var path1 = path.join(__dirname,'b')
 // console.log(path1)
--- app/index.js
+++ app/index.js
@@ -7,11 +7,15 @@ const path = require('path')
 const koaBody = require('koa-body')
 const bodyParser = require('koa-bodyparser')
 
+// 中间层，用来连接数据库
+const Monk = require('monk')
+const mongodb = Monk('localhost/runoob') // test就是你的数据库
+// 读取user集合
+const user = mongodb.get('site')
+
 // 使用ctx.body解析中间件
 app.use(koaBody());
 app.use(bodyParser());
-
-
 
 // 加载模板引擎
 app.use(views(path.join(__dirname, './views'), {
